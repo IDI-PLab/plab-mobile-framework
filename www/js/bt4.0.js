40e7ac113f546bd3e25d1587a760bde636500ef2
Sitron Te <halsaune@stud.ntnu.no>
2015-01-15 15:10:35 +0100

    Init btle inserted into bt4.0.js

--- www/js/bt4.0.js
+++ www/js/bt4.0.js
@@ -1,4 +1,29 @@
-function plabAddBT4_0() {
+/*
+ * 
+var plabBTMode = {
+		id : "",
+		name : "",
+		status : {
+			initialized : false,
+			connected : false,
+			ready : false,
+			failure : false
+		},
+		
+		openMode : function () {},
+		closeMode : function () {},
+		
+		listDevices : function (listCallback, scanTime) {},
+		connectDevice : function (id) {},
+		disconnectDevice : function () {},
+		
+		send : function (text) {},
+		receiveCallback : function (callback) {}
+}
+ *
+ */
+
+function plabAddBT4_0(debugOut, updateScreen) {
 	// See if necessary plugin is installed
 	if (typeof bluetoothle === "undefined") {
 		return;
@@ -7,16 +32,60 @@ function plabAddBT4_0() {
 	var btMode = Object.create(plabBTMode);
 	btMode.id = "BlueTooth_4.0_randdusing";
 	btMode.name = "BlueTooth LE";
+	// Adding info if this is first run
+	btMode.status.started = false;
 	// Replacing the open mode function -> init the mode
 	btMode.openMode = function () {
-		if (btMode.status.initialized) {
+		if (btMode.status.started) {
 			// TODO
 		} else {
 			// First run -> set up
+			btMode.closeMode : function () {
+				// Remove all listeners
+				// TODO
+				// Close open connections
+				btMode.disconnectDevice();
+				// Update status
+				btMode.status.connected = false;
+				btMode.status.ready = false;
+				btMode.status.failure = false;
+			};
+			
+			btMode.listDevices : function (listCallback, scanTime) {};
+			btMode.connectDevice : function (id) {};
+			btMode.disconnectDevice : function () {};
+			
+			btMode.send : function (text) {};
+			btMode.receiveCallback : function (callback) {};
 			// TODO
+			
+			// -------------- INIT -------------------
+			if (!btMode.status.initialized) {
+				bluetoothle.initialize(
+						
+						function(obj) {
+							if (obj.status == "enabled") {
+								btMode.status.initialized = true;
+								updateScreen();
+							} else {
+								debugOut.err.println("InitializeFailure: Unknown status: " + obj.status);
+							}
+						},
+						
+						function(obj) {
+							btMode.status.failure = true;
+							debugOut.err.println("InitializeFailure: " + obj.error + " - " + obj.message);
+							updateScreen();
+						},
+						
+						{"request":false}
+				);
+			}
+			// -------------- END INIT -------------------
 		}
-		// TODO
 	};
 	
 	plabBT.modes[plabBT.modes.length] = btMode;
 }
+
+plabAddBT4_0(plab.out, plab.updateScreen);
