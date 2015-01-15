547a7be191cf5eb22ccb295f2846bc869b0b5157
Sitron Te <halsaune@stud.ntnu.no>
2015-01-15 13:43:53 +0100

    added check for plugin for bluetoothle

--- www/js/bt4.0.js
+++ www/js/bt4.0.js
@@ -1,10 +1,20 @@
 function plabAddBT4_0() {
+	// See if necessary plugin is installed
+	if (typeof bluetoothle === "undefined") {
+		return;
+	}
 	// Creating a prototype object
 	var btMode = Object.create(plabBTMode);
 	btMode.id = "BlueTooth_4.0_randdusing";
 	btMode.name = "BlueTooth LE";
 	// Replacing the open mode function -> init the mode
 	btMode.openMode = function () {
+		if (btMode.status.initialized) {
+			// TODO
+		} else {
+			// First run -> set up
+			// TODO
+		}
 		// TODO
 	};
 	
