4c7917b720d79bf01cedd169f6508080b8414181
Sitron Te <halsaune@stud.ntnu.no>
2015-01-16 11:31:27 +0100

    Reduced logging level

--- www/js/index.js
+++ www/js/index.js
@@ -46,7 +46,7 @@ var plabPrintStream = {
 var plab = {
 		// ---------------- DEBUG OUTPUT ---------------------------
 		out : {
-			logLevel : 2,
+			logLevel : 0,
 			node : null,
 			notify : null,
 			warn : null,
