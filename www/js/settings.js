a1397e2e7e3891aabb1924aa55cb8f7d6b10dd82
Inge Edward Halsaunet <halsaune@stud.ntnu.no>
2016-04-26 16:56:14 +0200

    This should make settingsController an actual MVC controller with the possibility to add and remove listeners

--- www/js/settings.js
+++ www/js/settings.js
@@ -37,218 +37,271 @@ settingItem = {
 */
 
 plab.settingsController = {
-		visible : false,
-		showSettings : function () {
-			plab.settingsController.visible = true;
-			document.body.classList.add("plab-show-menu");
-		},
-		hideSettings : function () {
-			plab.settingsController.visible = false;
-			document.body.classList.remove("plab-show-menu");
-		},
-		toggleSettings : function () {
-			if (plab.settingsController.visible) {
-				plab.settingsController.hideSettings();
-			} else {
-				plab.settingsController.showSettings();
+
+	// Setting change listeners. DO NOT MANUALLY CHANGE. use add or remove listeners
+	changeListeners : {},
+	// Visibility state of setting menu
+	visible : false,
+
+	// Show or hide settings menu
+	showSettings : function () {
+		plab.settingsController.visible = true;
+		document.body.classList.add("plab-show-menu");
+	},
+	hideSettings : function () {
+		plab.settingsController.visible = false;
+		document.body.classList.remove("plab-show-menu");
+	},
+	toggleSettings : function () {
+		if (plab.settingsController.visible) {
+			plab.settingsController.hideSettings();
+		} else {
+			plab.settingsController.showSettings();
+		}
+	},
+
+	// Add a new item to settings menu
+	addSettingItem : function (item) {
+		var settingNode = this.createSettingNode(item);
+		if (item.type !== "group") {
+			val = this.getSettingValue(item.id);
+			if (val == null) {
+				if (typeof item["default-value"] !== "undefined") {
+					this.setSettingValue(item.id, item["default-value"]);
+				} else {
+					this.setSettingValue(item.id, "");
+				}
 			}
-		},
-		addSettingItem : function (item) {
-			var settingNode = this.createSettingNode(item);
-			if (item.type !== "group") {
-				val = this.getSettingValue(item.id);
-				if (val == null) {
-					if (typeof item["default-value"] !== "undefined") {
-						this.setSettingValue(item.id, item["default-value"]);
-					} else {
-						this.setSettingValue(item.id, "");
+		}
+		document.getElementById("plab-settings-menu").appendChild(settingNode);
+	},
+	
+	// Get or set a specific setting value
+	getSettingValue : function(id) {
+		return window.localStorage.getItem(id);
+	},
+	setSettingValue : function(id, val) {
+		// Check against old value
+		var oldVal = plab.settingsController.getSettingValue(id);
+		if (val !== oldVal) {
+			// Value has changed, store it and fire change events
+			window.localStorage.setItem(id,val);
+			if (typeof plab.settingsController.changeListeners[id] !== "undefined") {
+				for (var i = 0; i < plab.settingsController.changeListeners[id].length; i++) {
+					var l = plab.settingsController.changeListeners[id][i];
+					if (typeof l === "function") {
+						l(id, oldVal, val);
 					}
 				}
 			}
-			document.getElementById("plab-settings-menu").appendChild(settingNode);
-		},
-		
-		getSettingValue : function(id) {
-			return window.localStorage.getItem(id);
-		},
-		setSettingValue : function(id, val) {
-			window.localStorage.setItem(id,val);
-		},
-		
-		createSettingNode : function(item) {
-			var sett;
-			switch (item.type) {
-			case "single-select":
-				sett = this.createSingleSelectNode(item);
-				break;
-			case "text":
-			case "url":
-			case "number":
-			case "checkbox":
-				sett = this.createTextNumberCheckboxOrUrlNode(item);
-				break;
-			case "group":
-				var div = document.createElement("div");
-				this.createSettingHead(item, div);
-				for (var i = 0; i < item.options.length; i++) {
-					div.appendChild(this.createSettingNode(item.options[i]));
-				}
-				sett = div;
-				break;
-			default:
-				throw "Unkown setting type";
-			}
-			
-			if (typeof item["description-text-key"] !== "undefined") {
-				var spn = document.createElement("span");
-				var key = item["description-text-key"];
-				spn.className = "plab-setting-description";
-				this.createSetAttributeNode("data-text-key", key, spn);
-				var txt = document.createTextNode(plabLangSupport.getText(key));
-				spn.appendChild(txt);
-				sett.appendChild(spn);
-			}
-			
-			return sett;
-		},
-		createSettingHead : function(item, holderNode) {
-			holderNode.className = "plab-setting";
-			
-			if (typeof item["text-key"] !== "undefined") {
-				holderNode.appendChild(this.createHeaderTextKeyNode(item["text-key"]));
-			}
-			if (typeof item["icon"] !== "undefined") {
-				holderNode.appendChild(this.createImageNode(item["icon"]));
-			}
-		},
-		createSingleSelectNode : function(item) {
+		}
+	},
+
+	// Add or remove listeners to setting change
+	addChangeListener : function(id, listener) {
+		if (typeof plab.settingsController.changeListeners[id] === "undefined") {
+			plab.settingsController.changeListeners[id] = [];
+		}
+		plab.settingsController.changeListeners[id][plab.settingsController.changeListeners[id].length] = listener;
+	},
+	removeChangeListener : function(id, listener) {
+		if (typeof plab.settingsController.changeListeners[id] === "undefined") { return; }
+		var i = plab.settingsController.changeListeners[id].indexOf(listener);
+		if (i > -1) {
+			plab.settingsController.changeListeners[id].splice(i, 1);
+		}
+	},
+	
+	// Internal functions that are used for menu setting creation
+	createSettingNode : function(item) {
+		var sett;
+		switch (item.type) {
+		case "single-select":
+			sett = this.createSingleSelectNode(item);
+			break;
+		case "text":
+		case "url":
+		case "number":
+		case "checkbox":
+			sett = this.createTextNumberCheckboxOrUrlNode(item);
+			break;
+		case "group":
 			var div = document.createElement("div");
 			this.createSettingHead(item, div);
-			
-			var val = this.getSettingValue(item.id);
-			if (val === null){
-				val = typeof item["default-value"] !== "undefined" ? item["default-value"] : "";
-				this.setSettingValue(item.id, val);
-			}
-			
-			var sel = document.createElement("select");
 			for (var i = 0; i < item.options.length; i++) {
-				sel.appendChild(this.createSingleOptionNode(item.options[i], item.options[i].value == val));
+				div.appendChild(this.createSettingNode(item.options[i]));
 			}
-			
-			this.createSetAttributeNode("id", item.id, sel);
-			
-			sel.addEventListener("change", function() {
-				var str = document.getElementById(item.id).value;
-				window.localStorage.setItem(item.id, str);
-				if (typeof item.onValueChange === "function")
-					item.onValueChange(str);
-			});
-			
-			div.appendChild(sel);
-			return div;
-		},
-		createTextNumberCheckboxOrUrlNode : function(item) {
-			var div = document.createElement("div");
-			this.createSettingHead(item,div);
-			
-			if (typeof item.options[0]["text-key"] !== "undefined") {
-				div.appendChild(this.createLabelTextKeyNode(item.options[0]["text-key"], item.id));
-			}
-			if (typeof item.options[0]["icon"] !== "undefined") {
-				div.appendChild(this.createImageNode(item.options[0]["icon"]));
-			}
-			var inp = document.createElement("input");
-			this.createSetAttributeNode("type", item.type, inp);
-			this.createSetAttributeNode("id", item.id, inp);
-			
-			if (item.type === "number") {
-				if (typeof item.options[0]["min"] !== "undefined") {
-					this.createSetAttributeNode("min", item.options[0]["min"], inp)
-				}
-				if (typeof item.options[0]["max"] !== "undefined") {
-					this.createSetAttributeNode("max", item.options[0]["max"], inp)
-				}
-			}
-			
-			var val = this.getSettingValue(item.id);
-			if (val === null){
-				if (item.type === "checkbox") {
-					val = typeof item["default-value"] !== "undefined" ? item["default-value"] : "false";
-				} else {
-					val = typeof item["default-value"] !== "undefined" ? item["default-value"] : "";
-				}
-				this.setSettingValue(item.id, val);
-			}
-			if (item.type === "checkbox") {
-				val = val == "true";	// Javascript is nice...
-				inp.checked = val;
-			} else {
-				this.createSetAttributeNode("value", val, inp);
-			}
-			
-			inp.addEventListener("change", function() {
-				var str = "";
-				if (item.type === "checkbox") {
-					str = document.getElementById(item.id).checked ? "true" : "false";
-				} else {
-					str = document.getElementById(item.id).value;
-				}
-				window.localStorage.setItem(item.id, str);
-				if (typeof item.onValueChange === "function")
-					item.onValueChange(str);
-			});
-			
-			div.appendChild(inp);
-			return div;
-		},
+			sett = div;
+			break;
+		default:
+			throw "Unkown setting type";
+		}
 		
-		createSingleOptionNode : function(option, selected) {
-			var opt = document.createElement("option");
-			this.createSetAttributeNode("value", option["value"], opt);
-			if (selected) {
-				this.createSetAttributeNode("selected", "true", opt);
-			}
-			if (typeof option["text-key"] !== "undefined") {
-				opt.appendChild(this.createDataTextKeyNode(option["text-key"]));
-			}
-			if (typeof option["icon"] !== "undefined") {
-				opt.appendChild(this.createImageNode(option["icon"]));
-			}
-			return opt;
-		},
-		
-		createDataTextKeyNode : function(key) {
+		if (typeof item["description-text-key"] !== "undefined") {
 			var spn = document.createElement("span");
+			var key = item["description-text-key"];
+			spn.className = "plab-setting-description";
 			this.createSetAttributeNode("data-text-key", key, spn);
 			var txt = document.createTextNode(plabLangSupport.getText(key));
 			spn.appendChild(txt);
-			return spn;
-		},
-		createLabelTextKeyNode : function(key, name) {
-			var lbl = document.createElement("label");
-			this.createSetAttributeNode("data-text-key", key, lbl);
-			this.createSetAttributeNode("for", name, lbl);
-			var txt = document.createTextNode(plabLangSupport.getText(key));
-			lbl.appendChild(txt);
-			return lbl;
-		},
-		createImageNode : function(location) {
-			var im = document.createElement("img");
-			this.createSetAttributeNode("src", location, im);
-			return im;
-		},
-		createHeaderTextKeyNode : function(key) {
-			var h = document.createElement("h3");
-			this.createSetAttributeNode("data-text-key", key, h);
-			var txt = document.createTextNode(plabLangSupport.getText(key));
-			h.appendChild(txt);
-			return h;
-		},
-		
-		createSetAttributeNode : function (attribute, value, element) {
-			var att = document.createAttribute(attribute);
-			att.value = value;
-			element.setAttributeNode(att);
+			sett.appendChild(spn);
 		}
+		
+		return sett;
+	},
+	createSettingHead : function(item, holderNode) {
+		holderNode.className = "plab-setting";
+		
+		if (typeof item["text-key"] !== "undefined") {
+			holderNode.appendChild(this.createHeaderTextKeyNode(item["text-key"]));
+		}
+		if (typeof item["icon"] !== "undefined") {
+			holderNode.appendChild(this.createImageNode(item["icon"]));
+		}
+	},
+	createSingleSelectNode : function(item) {
+		var div = document.createElement("div");
+		this.createSettingHead(item, div);
+		
+		var val = this.getSettingValue(item.id);
+		if (val === null){
+			val = typeof item["default-value"] !== "undefined" ? item["default-value"] : "";
+			this.setSettingValue(item.id, val);
+		}
+		
+		var sel = document.createElement("select");
+		for (var i = 0; i < item.options.length; i++) {
+			sel.appendChild(this.createSingleOptionNode(item.options[i], item.options[i].value == val));
+		}
+		
+		this.createSetAttributeNode("id", item.id, sel);
+
+		// MVC pattern: add listener to setting controller
+		plab.settingsController.addChangeListener(item.id, function(id, oldVal, newVal) {
+			document.getElementById(id).value = newVal;
+		});
+		
+		sel.addEventListener("change", function() {
+			var str = document.getElementById(item.id).value;
+			window.localStorage.setItem(item.id, str);
+			if (typeof item.onValueChange === "function")
+				item.onValueChange(str);
+		});
+		
+		div.appendChild(sel);
+		return div;
+	},
+	createTextNumberCheckboxOrUrlNode : function(item) {
+		var div = document.createElement("div");
+		this.createSettingHead(item,div);
+		
+		if (typeof item.options[0]["text-key"] !== "undefined") {
+			div.appendChild(this.createLabelTextKeyNode(item.options[0]["text-key"], item.id));
+		}
+		if (typeof item.options[0]["icon"] !== "undefined") {
+			div.appendChild(this.createImageNode(item.options[0]["icon"]));
+		}
+		var inp = document.createElement("input");
+		this.createSetAttributeNode("type", item.type, inp);
+		this.createSetAttributeNode("id", item.id, inp);
+		
+		if (item.type === "number") {
+			if (typeof item.options[0]["min"] !== "undefined") {
+				this.createSetAttributeNode("min", item.options[0]["min"], inp)
+			}
+			if (typeof item.options[0]["max"] !== "undefined") {
+				this.createSetAttributeNode("max", item.options[0]["max"], inp)
+			}
+		}
+		
+		var val = this.getSettingValue(item.id);
+		if (val === null){
+			if (item.type === "checkbox") {
+				val = typeof item["default-value"] !== "undefined" ? item["default-value"] : "false";
+			} else {
+				val = typeof item["default-value"] !== "undefined" ? item["default-value"] : "";
+			}
+			this.setSettingValue(item.id, val);
+		}
+		if (item.type === "checkbox") {
+			val = val == "true";	// Javascript is nice...
+			inp.checked = val;
+		} else {
+			this.createSetAttributeNode("value", val, inp);
+		}
+
+		// MVC pattern: add listener to setting controller
+		plab.settingsController.addChangeListener(item.id, function(id, oldVal, newVal) {
+			var el = document.getElementById(id);
+			if (el.type == "checkbox") {
+				el.checked = newVal == "true";
+			} else {
+				el.value = newVal;
+			}
+		});
+		
+		inp.addEventListener("change", function() {
+			var str = "";
+			if (item.type === "checkbox") {
+				str = document.getElementById(item.id).checked ? "true" : "false";
+			} else {
+				str = document.getElementById(item.id).value;
+			}
+			window.localStorage.setItem(item.id, str);
+			if (typeof item.onValueChange === "function")
+				item.onValueChange(str);
+		});
+		
+		div.appendChild(inp);
+		return div;
+	},
+	
+	createSingleOptionNode : function(option, selected) {
+		var opt = document.createElement("option");
+		this.createSetAttributeNode("value", option["value"], opt);
+		if (selected) {
+			this.createSetAttributeNode("selected", "true", opt);
+		}
+		if (typeof option["text-key"] !== "undefined") {
+			opt.appendChild(this.createDataTextKeyNode(option["text-key"]));
+		}
+		if (typeof option["icon"] !== "undefined") {
+			opt.appendChild(this.createImageNode(option["icon"]));
+		}
+		return opt;
+	},
+	
+	createDataTextKeyNode : function(key) {
+		var spn = document.createElement("span");
+		this.createSetAttributeNode("data-text-key", key, spn);
+		var txt = document.createTextNode(plabLangSupport.getText(key));
+		spn.appendChild(txt);
+		return spn;
+	},
+	createLabelTextKeyNode : function(key, name) {
+		var lbl = document.createElement("label");
+		this.createSetAttributeNode("data-text-key", key, lbl);
+		this.createSetAttributeNode("for", name, lbl);
+		var txt = document.createTextNode(plabLangSupport.getText(key));
+		lbl.appendChild(txt);
+		return lbl;
+	},
+	createImageNode : function(location) {
+		var im = document.createElement("img");
+		this.createSetAttributeNode("src", location, im);
+		return im;
+	},
+	createHeaderTextKeyNode : function(key) {
+		var h = document.createElement("h3");
+		this.createSetAttributeNode("data-text-key", key, h);
+		var txt = document.createTextNode(plabLangSupport.getText(key));
+		h.appendChild(txt);
+		return h;
+	},
+	
+	createSetAttributeNode : function (attribute, value, element) {
+		var att = document.createAttribute(attribute);
+		att.value = value;
+		element.setAttributeNode(att);
+	}
 }
